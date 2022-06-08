577566b6ad1ce78c6ce714a12be7ad7e320545fc
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-08 21:18:25 +0530

    Add files via upload

--- currency converter.py
+++ currency converter.py
@@ -1,8 +1,8 @@
 from forex_python.converter import CurrencyRates
 c=CurrencyRates()
-amount=int(input("Enter the amount: "))
+amount=float(input("Enter the amount: "))
 from_currency=input("From Currency: ").upper()
 to_currency=input("To currency: ").upper()
-print(from_currency,"To",to_currency,amount)
+print(from_currency,"To",to_currency)
 result=c.convert(from_currency,to_currency,amount)
 print(result)
