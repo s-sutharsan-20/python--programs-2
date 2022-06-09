7e78630ad81e691ec69fd7acfcc897237c8fdf3d
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-09 19:04:09 +0530

    Add files via upload

---
+++ statistics.py
@@ -0,0 +1,5 @@
+import numpy as np
+data=np.array([475,435,789,507,356,665,888,953,435,670,346,876,467,554,230])
+print("Standard Deviation :",np.std(data))
+print("Mean :",np.mean(data))
+print("Median :",np.median(data))
