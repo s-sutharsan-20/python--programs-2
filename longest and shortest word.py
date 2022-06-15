49d434d4ba08a64abad0288e7b6ba43a8e72d3f9
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-15 18:29:51 +0530

    Add files via upload

---
+++ longest and shortest word.py
@@ -0,0 +1,5 @@
+words=['python','numpy','sql','pandas','matplotlib']
+longest_word=max(words,key=len)
+shortest_word=min(words,key=len)
+print("Longest word in the list is  :",longest_word)
+print("Shortest word in the list is  :",shortest_word)
