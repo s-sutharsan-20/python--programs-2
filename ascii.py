f4d6d2f64d7894896b7e034584df04c077b7b259
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-15 18:29:33 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,15 @@
+import string
+
+alphabets=string.ascii_letters
+lowercase_alphabets=string.ascii_lowercase
+uppercase_alphabets=string.ascii_uppercase
+numbers=string.digits
+
+
+print("\nAlphabets :",list(alphabets))
+
+print("\nAlphabets in uppercase letters :",list(uppercase_alphabets))
+
+print("\nAlphabets in lowercase letters :",list(lowercase_alphabets))
+
+print("\nNumbers :",list(numbers))
