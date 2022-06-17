463772fad18815cee9ce9494211ff4b24de9fcc7
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-17 22:48:29 +0530

    Add files via upload

---
+++ percentage calculation.py
@@ -0,0 +1,4 @@
+total=float(input("Enter the total number :"))
+condition=float(input("Enter the condition :"))
+percentage=condition/total*100
+print(condition,"  percent of :",total,"is :",percentage)
